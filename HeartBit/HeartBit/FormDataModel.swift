0623f6e4269b3c0357cf19c537da0b9f562dbce8
Diogo C Brunoro <161259842+DiogoBrunoro@users.noreply.github.com>
2025-04-28 21:48:56 -0300

    Delete HeartBit directory

--- HeartBit/HeartBit/FormDataModel.swift
+++
@@ -1,7 +0,0 @@
-//
-//  FormDataModel.swift
-//  HeartBit
-//
-//  Created by Turma01-7 on 09/04/25.
-//
-
--- HeartBit/Model.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Model.swift
-//  HeartBit
-//
-//  Created by Turma01-7 on 08/04/25.
-//
-
-import Foundation
